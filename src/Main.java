63e3040e5a52c8db9643d4d6b0910a2d7ca764f8
dashoes15 <daltonasali@gmail.com>
2023-10-11 09:23:25 -0400

    lab 6 task 3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+import java.lang.Math;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        double length;
+        double width;
+        boolean done = false;
+        System.out.println("Enter the sides of the rectangle!");
+        do {
+            System.out.println("Length:");
+            if (scan.hasNextDouble()) {
+                length = scan.nextDouble();
+                System.out.println("Width:");
+                if (scan.hasNextDouble()) {
+                    width = scan.nextDouble();
+                    if (width > 0 && length > 0) {
+                        System.out.printf("%-13s%13.2f", "Area:", length*width);
+                        System.out.printf("\n%-13s%13.2f", "Perimeter:", length*2 + width*2);
+                        System.out.printf("\n%-13s%13.2f", "Length of diaganol:", Math.sqrt(Math.pow(length, 2) + Math.pow(width, 2)));
+                        done = true;
+                    }else{
+                        System.out.println("You have entered something negative!");
+                        scan.nextLine();
+                    }
+                }else{
+                    System.out.println("Oops! You entered something invalid!");
+                    scan.nextLine();
+                }
+            }else{
+                System.out.println("Oops! You entered something invalid!");
+                scan.nextLine();
+            }
+        } while (!done);
     }
 }
